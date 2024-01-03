6ff8ec2b3f372e7857a6c58c2d02ea891eb1d0f0
heroq <96118954+heroq@users.noreply.github.com>
2024-01-03 21:56:16 +0900

    init

---
+++ src/main/java/com/hh99/level2/Hh99Level2Application.java
@@ -0,0 +1,13 @@
+package com.hh99.level2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hh99Level2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hh99Level2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hh99/level2/Hh99Level2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hh99.level2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Hh99Level2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
