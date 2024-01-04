6502c6fd905a4706eb6c8dd94ca5500aa9f0f894
heroq <96118954+heroq@users.noreply.github.com>
2024-01-04 18:24:12 +0900

    Fix: 도서 상세 조회만 id 응답

--- src/main/java/com/hh99/level2/dto/BookResponseDto.java
+++ src/main/java/com/hh99/level2/dto/BookResponseDto.java
@@ -2,13 +2,17 @@ package com.hh99.level2.dto;
 
 import com.hh99.level2.entity.Book;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.Setter;
 
 import java.time.LocalDateTime;
 
-@Getter
 @AllArgsConstructor
+@Getter
 public class BookResponseDto {
+    @Setter
+    private Long id;
     private String title;
     private String author;
     private String lang;
