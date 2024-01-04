1f1b09702d8cf412ce8ff9112da0750935dfae3a
heroq <96118954+heroq@users.noreply.github.com>
2024-01-04 16:28:42 +0900

    Feat: "도서 엔티티, DTO 추가"

--- src/main/java/com/hh99/level2/dto/BookRequestDto.java
+++ src/main/java/com/hh99/level2/dto/BookRequestDto.java
@@ -1,4 +1,18 @@
 package com.hh99.level2.dto;
 
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@AllArgsConstructor
 public class BookRequestDto {
+    private Long id;
+    private String title;
+    private String author;
+    private String lang;
+    private String company;
+    private LocalDateTime regDate;
 }
--- src/main/java/com/hh99/level2/entity/Book.java
+++ src/main/java/com/hh99/level2/entity/Book.java
@@ -1,4 +1,41 @@
 package com.hh99.level2.entity;
 
+import com.hh99.level2.dto.BookRequestDto;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Table(name="books")
+@NoArgsConstructor
 public class Book {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String title;
+
+    @Column
+    private String author;
+
+    @Column
+    private String lang;
+
+    @Column
+    private String company;
+
+    @Column(name="reg_date")
+    private LocalDateTime regDate;
+
+    public Book(BookRequestDto bookRequestDto){
+        this.title = bookRequestDto.getTitle();
+        this.author = bookRequestDto.getAuthor();
+        this.lang = bookRequestDto.getLang();
+        this.company = bookRequestDto.getCompany();
+        this.regDate = bookRequestDto.getRegDate();
+    }
 }
